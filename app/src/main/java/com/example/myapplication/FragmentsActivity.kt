a917e41ce524102db0a247ea0ecfd5426c4ea96a
laplasian <nifilimity@gmail.com>
2025-10-07 10:57:57 +0700

    last

--- app/src/main/java/com/example/myapplication/FragmentsActivity.kt
+++ app/src/main/java/com/example/myapplication/FragmentsActivity.kt
@@ -8,14 +8,20 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.example.myapplication.Auth
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    private val authViewModel: AuthViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val username = view.findViewById<EditText>(R.id.username)
         val password = view.findViewById<EditText>(R.id.password)
         val loginBtn = view.findViewById<Button>(R.id.login)
+
+        val AUTH = authViewModel.auth
 
         val textWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -23,7 +29,14 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 val usernameInput = username.text.toString().trim()
                 val passwordInput = password.text.toString().trim()
-                loginBtn.isEnabled = usernameInput.isNotEmpty() && passwordInput.isNotEmpty()
+                if (usernameInput.isNotEmpty() && passwordInput.isNotEmpty()){
+                    loginBtn.isEnabled = true
+                    if (AUTH.findUser(username.text?.toString().orEmpty())) {
+                        loginBtn.text = "Sing in"
+                    } else {
+                        loginBtn.text = "Register"
+                    }
+                }
             }
 
             override fun afterTextChanged(s: Editable?) {}
@@ -36,8 +49,18 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             val username_ = username.text?.toString().orEmpty()
             val password_ = password.text?.toString().orEmpty()
 
-            val args = bundleOf("username" to username_, "password" to password_)
-            findNavController().navigate(R.id.action_loginFragment_to_main, args)
+            if (loginBtn.text == "Sing in") {
+                if (AUTH.login(username_, password_)){
+                    val args = bundleOf("username" to username_, "password" to password_)
+                    findNavController().navigate(R.id.action_loginFragment_to_main, args)
+                }
+            }
+
+            if (loginBtn.text == "Register") {
+                AUTH.add(User(username_, password_))
+                val args = bundleOf("username" to username_, "password" to password_)
+                findNavController().navigate(R.id.action_loginFragment_to_main, args)
+            }
         }
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.fragment.app.activityViewModels
 
-
+class AuthViewModel : ViewModel() {
+    val auth = Auth().apply { add(User("admin", "admin")) }
+}
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val authViewModel by viewModels<AuthViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
